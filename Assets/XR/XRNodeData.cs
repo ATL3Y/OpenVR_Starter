c1be649932dcc25bdbae2d89494a8e92543923be
Atley Loughridge <atleyloughridge@Atleys-MBP.jetpack>
2019-01-30 15:37:59 -0800

    First commit.

---
+++ Assets/XR/XRNodeData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class XRNodeData: MonoBehaviour
+{
+    public UnityEngine.XR.XRNode XRNode { get; set; }
+    public Camera XRCamera { get; set; }
+
+    // For more information on OpenVR Controllers, see: 
+    // https://docs.unity3d.com/2017.1/Documentation/Manual/OpenVRControllers.html
+
+    // Oculus: B or Y, Vive: Menu
+    public bool ButtonPress { get; set; }
+    public bool ButtonPressDown { get; set; }
+    public bool ButtonPressUp { get; set; }
+
+    // Oculus: Thumbstick, Vive: Trackpad
+    public bool ThumbPress { get; set; }
+    public bool ThumbPressDown { get; set; }
+    public bool ThumbPressUp { get; set; }
+    public bool ThumbTouch { get; set; }
+    public bool ThumbTouchDown { get; set; }
+    public bool ThumbTouchUp { get; set; }
+    public Vector2 ThumbPos { get; set; }
+
+    // Oculus: Index Trigger, Vive: Trigger
+    public float IndexTriggerSqueeze { get; set; }
+    public bool IndexTriggerTouch { get; set; }
+    public bool IndexTriggerTouchDown { get; set; }
+    public bool IndexTriggerTouchUp { get; set; }
+
+    // Oculus: Hand Trigger, Vive: Hand Grip
+    public float HandTriggerSqueeze { get; set; }
+
+    // Custom index trigger usage.
+    public bool IndexTrigger { get; set; }
+    public bool IndexTriggerDown { get; set; } 
+    public bool IndexTriggerUp { get; set; } 
+
+    public XRRig XRRig { get; set; }
+    private bool debug = true;
+
+    private bool isHead = false;
+
+    public void Init ()
+    {
+        isHead = name.Contains ( "Head" );
+        if ( isHead )
+        {
+
+        }
+    }
+}
---
+++ Assets/XR/XRRig.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRRig: MonoBehaviour
+{
+    public XRNodeData head;
+    public XRNodeData leftHand;
+    public XRNodeData rightHand;
+
+    private List<UnityEngine.XR.XRNodeState> xrNodeStates = new List<UnityEngine.XR.XRNodeState>();
+
+    public void Init ( bool mine )
+    {
+        Debug.Assert ( head.GetComponent<Camera> ( ) != null, "Head camera component is null" );
+        Debug.Assert ( head.GetComponent<AudioListener> ( ) != null, "Head audio listener component is null" );
+
+        head.Init ( );
+        leftHand.Init ( );
+        rightHand.Init ( );
+        // me = mine;
+
+        if ( mine )
+        {
+            head.GetComponent<Camera> ( ).enabled = true;
+            head.XRCamera = head.GetComponent<Camera> ( );
+            head.gameObject.tag = "MainCamera";
+
+            head.GetComponent<AudioListener> ( ).enabled = true;
+
+            leftHand.XRNode = UnityEngine.XR.XRNode.LeftHand;
+            rightHand.XRNode = UnityEngine.XR.XRNode.RightHand;
+        }
+        else
+        {
+            head.GetComponent<Camera> ( ).enabled = false;
+            head.GetComponent<AudioListener> ( ).enabled = false;
+        }
+    }
+
+    public void XRRigUpdate ( )
+    {
+        UnityEngine.XR.InputTracking.GetNodeStates ( xrNodeStates );
+        SetTransforms ( );
+    }
+
+    private bool IsTracking ( UnityEngine.XR.XRNode xrNode )
+    {
+        for ( int i = 0; i < xrNodeStates.Count; i++ )
+        {
+            if ( xrNodeStates [ i ].nodeType == xrNode )
+            {
+                return xrNodeStates [ i ].tracked;
+            }
+        }
+        return false;
+    }
+
+    private void UpdateTransformFromTracking ( Transform tran, XRNode xrNode )
+    {
+        if ( IsTracking ( xrNode ) )
+        {
+            tran.localPosition = InputTracking.GetLocalPosition ( xrNode );
+            tran.localRotation = InputTracking.GetLocalRotation ( xrNode );
+        }
+    }
+
+    private void SetTransforms ( )
+    {
+        if ( !UnityEngine.XR.XRDevice.isPresent )
+            return;
+
+        UpdateTransformFromTracking ( head.transform, XRNode.Head );
+        UpdateTransformFromTracking ( leftHand.transform, XRNode.LeftHand );
+        UpdateTransformFromTracking ( rightHand.transform, XRNode.RightHand );
+    }
+}
